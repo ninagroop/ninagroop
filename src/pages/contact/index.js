4c3f8255f3ee6c7994246493a4015537137a82e5
Brian Feister <bfeister@salesforce.com>
2021-12-06 12:09:26 -0500

    Fix contact page width issue

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -41,75 +41,74 @@ const Contact = ({ location }) => {
       <div className="article-body">
         <section className="section">
           <div className="container">
-            <div className="content">
-              <form
-                name="contact"
-                method="post"
-                action="/contact/thanks/"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                onSubmit={handleSubmit}
-              >
-                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="form-name" value="contact" />
-                <div hidden>
-                  <label>
-                    Don’t fill this out:{' '}
-                    <input name="bot-field" onChange={handleChange} />
-                  </label>
+            <form
+              name="contact"
+              method="post"
+              action="/contact/thanks/"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              onSubmit={handleSubmit}
+            >
+              {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+              <input type="hidden" name="form-name" value="contact" />
+              <div hidden>
+                <label>
+                  Don’t fill this out:{' '}
+                  <input name="bot-field" onChange={handleChange} />
+                </label>
+              </div>
+              <div className="field">
+                <label className="label" htmlFor={'name'}>
+                  Your name
+                </label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type={'text'}
+                    name={'name'}
+                    onChange={handleChange}
+                    id={'name'}
+                    required={true}
+                  />
                 </div>
-                <div className="field">
-                  <label className="label" htmlFor={'name'}>
-                    Your name
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={'text'}
-                      name={'name'}
-                      onChange={handleChange}
-                      id={'name'}
-                      required={true}
-                    />
-                  </div>
+              </div>
+              <div className="field">
+                <label className="label" htmlFor={'email'}>
+                  Email
+                </label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type={'email'}
+                    name={'email'}
+                    onChange={handleChange}
+                    id={'email'}
+                    required={true}
+                  />
                 </div>
-                <div className="field">
-                  <label className="label" htmlFor={'email'}>
-                    Email
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={'email'}
-                      name={'email'}
-                      onChange={handleChange}
-                      id={'email'}
-                      required={true}
-                    />
-                  </div>
+              </div>
+              <div className="field">
+                <label className="label" htmlFor={'message'}>
+                  Message
+                </label>
+                <div className="control">
+                  <textarea
+                    className="textarea"
+                    name={'message'}
+                    onChange={handleChange}
+                    id={'message'}
+                    required={true}
+                  />
                 </div>
-                <div className="field">
-                  <label className="label" htmlFor={'message'}>
-                    Message
-                  </label>
-                  <div className="control">
-                    <textarea
-                      className="textarea"
-                      name={'message'}
-                      onChange={handleChange}
-                      id={'message'}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <br />
-                <div className="field">
-                  <button className="button is-link" type="submit">
-                    Send
-                  </button>
-                </div>
-              </form>
-            </div>
+              </div>
+              <br />
+              <div className="field">
+                <button className="button is-link" type="submit">
+                  Send
+                </button>
+              </div>
+            </form>
+
             <br style={{ display: 'block', clear: 'both' }} />
           </div>
         </section>
